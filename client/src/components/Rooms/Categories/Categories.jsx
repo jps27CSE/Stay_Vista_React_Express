aa9ee2480a7dd8e1e26607697d515076ff195029
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-13 14:05:22 +0600

    Part 4

--- client/src/components/Rooms/Categories/Categories.jsx
+++ client/src/components/Rooms/Categories/Categories.jsx
@@ -1,10 +1,24 @@
+import { useSearchParams } from "react-router-dom";
 import Container from "../../Shared/Container";
 import { categories } from "./CategoriesData";
+import CategoryBox from "./CategoryBox";
 
 const Categories = () => {
+  const [params, setParams] = useSearchParams();
+  const category = params.get("category");
+  console.log(category);
   return (
     <Container>
-      <div>{categories.length}</div>
+      <div className="pt-4 flex items-center justify-between overflow-x-auto">
+        {categories.map((categoryItem) => (
+          <CategoryBox
+            key={categoryItem.label}
+            label={categoryItem.label}
+            icon={categoryItem.icon}
+            selected={category === categoryItem.label}
+          />
+        ))}
+      </div>
     </Container>
   );
 };
--- client/src/components/Rooms/Categories/CategoryBox.jsx
+++ client/src/components/Rooms/Categories/CategoryBox.jsx
@@ -1,6 +1,35 @@
 // eslint-disable-next-line react/prop-types
-const CategoryBox = ({ label, icon: Icon }) => {
-  return <div>{label}</div>;
+import { useNavigate, useSearchParams } from "react-router-dom";
+import qs from "query-string";
+const CategoryBox = ({ label, icon: Icon, selected }) => {
+  const [params, setParams] = useSearchParams();
+  const navigate = useNavigate();
+  const handleClick = () => {
+    const currentQuery = qs.parse(params.toString());
+    const updatedQuery = { ...currentQuery, category: label };
+
+    const url = qs.stringifyUrl({
+      url: "/",
+      query: updatedQuery,
+    });
+
+    navigate(url);
+  };
+  params.get("category");
+  return (
+    <div
+      onClick={handleClick}
+      className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer ${
+        selected
+          ? "border-b-neutral-800 text-neutrail-800"
+          : "border-transparent text-neutral-500"
+      }
+      `}
+    >
+      <Icon size={26} />
+      <div className="text-sm font-medium"> {label}</div>
+    </div>
+  );
 };
 
 export default CategoryBox;
