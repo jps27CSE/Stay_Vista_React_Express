b3faf8b6ecbae2d01da71c9f6c8379e78e473d02
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-17 17:11:10 +0600

    Part 2-1

--- client/src/pages/RoomDetails/RoomDetails.jsx
+++ client/src/pages/RoomDetails/RoomDetails.jsx
@@ -3,6 +3,10 @@ import Container from "../../components/Shared/Container";
 import { useParams } from "react-router-dom";
 import Loader from "../../components/Shared/Loader";
 import { Helmet } from "react-helmet-async";
+import Header from "../../components/RoomDetails/Header";
+import RoomInfo from "../../components/RoomDetails/RoomInfo";
+import Calender from "../../components/RoomDetails/Calender";
+import RoomReservation from "../../components/RoomDetails/RoomReservation";
 
 const RoomDetails = () => {
   const { id } = useParams();
@@ -26,10 +30,17 @@ const RoomDetails = () => {
       <Helmet>
         <title>{room?.title}</title>
       </Helmet>
-      <div className="">
-        <div className="flex flex-col gap-6">{/* Header */}</div>
-        <div className="">{/* Room Info */}</div>
-        {/* Calender */}
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <Header room={room} />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6 ">
+          <RoomInfo room={room} />
+          {/* Calender */}
+          <div className="md:col-span-3 order-first md:order-last mb-10">
+            <RoomReservation />
+          </div>
+        </div>
       </div>
     </Container>
   );
