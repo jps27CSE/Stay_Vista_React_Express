829e5e23bd4ca16429662e950d34327ebb2af400
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-19 16:27:25 +0600

    Part 3-3

--- client/src/api/rooms.js
+++ client/src/api/rooms.js
@@ -11,3 +11,9 @@ export const getRoom = async (id) => {
   const { data } = await axiosSecure.get(`/room/${id}`);
   return data;
 };
+
+// Save a room data in db
+export const addRoom = async (roomData) => {
+  const { data } = await axiosSecure.post(`/rooms`, roomData);
+  return data;
+};
