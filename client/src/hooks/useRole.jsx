d61814610dd8c1346b8e2165208e724a8cc53e9a
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2023-11-21 03:02:05 +0600

    Part 3-5

--- client/src/hooks/useRole.jsx
+++ client/src/hooks/useRole.jsx
@@ -1,27 +1,29 @@
 import { getRole } from "../api/auth";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import useAuth from "./useAuth";
 const useRole = () => {
-  // const { user, loading } = useAuth();
-  // const { data: role, isLoading } = useQuery({
-  //   enabled: !loading && !!user?.email,
-  //   queryFn: async () => await getRole(user?.email),
-  //   queryKey: ["role"],
-  // });
-
-  // return [role, isLoading];
-
   const { user, loading } = useAuth();
-  const [role, setRole] = useState(null);
+  const { data: role, isLoading } = useQuery({
+    enabled: !loading && !!user?.email,
+    queryFn: async () => await getRole(user?.email),
+    queryKey: ["role"],
+  });
 
-  useEffect(() => {
-    getRole(user?.email).then((data) => {
-      setRole(data);
-    });
-  }, [user]);
+  return [role, isLoading];
 
-  return [role];
+  // const { user } = useAuth();
+  // const [role, setRole] = useState(null);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   getRole(user?.email).then((data) => {
+  //     setRole(data);
+  //     setLoading(false);
+  //   });
+  // }, [user]);
+
+  // return [role, loading];
 };
 
 export default useRole;
